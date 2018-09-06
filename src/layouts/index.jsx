b1570bcf517c169fe84c4fc82a62b49da469b3fd
Yasunari Momoi <momo@iij.ad.jp>
2018-09-06 17:44:51 +0900

    update article title

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -8,7 +8,7 @@ class Layout extends React.Component {
 
     return (
       <div className="layout">
-        <Helmet defaultTitle="Blog by John Doe" />
+        <Helmet defaultTitle="momo's memorandums" />
         {children()}
       </div>
     );
